12df0ed9468a1daed0f6a82416321645a189bbd2
Gabriel Raul <gbielraul@gmail.com>
2021-12-07 14:00:01 -0400

    Path changes

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var http = require('http');
 var static = require('node-static');
 
-var path = new static.Server(`${__dirname}/localhost`);
+var path = new static.Server(`${__dirname}`);
 
-http.createServer(function (req, res) {
-    req.addListener('end', function() {
-        path.serve(req, res);
-    }).resume();
+http.createServer(function(req, res) {
+  req.addListener('end', function() {
+    path.serve(req, res);
+  }).resume();
 }).listen(8080);
 
 console.log('Server criado em: localhost:8080')
